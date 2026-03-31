be38ac3b49e5c16f8ab156454774b972a7de4ad0
shreyansh699 <shreyanshvaddoriya@gmail.com>
2026-03-31 14:33:00 +0530

    Add multiple new stocks to the stock data
    Added new stocks: HDFC Bank, ICICI Bank, SBI, Bharti Airtel, Wipro, HCL Tech, Larsen & Toubro, ITC, Maruti Suzuki, Tata Motors, Sun Pharma, ONGC, NTPC, Adani Enterprises, Asian Paints.

--- script.js
+++ script.js
@@ -14,6 +14,81 @@ const stocks = {
         sector: "IT",
         prices: [1400, 1450, 1500, 1480, 1520]
     }
+    hdfc: {
+    name: "HDFC Bank",
+    sector: "Banking",
+    prices: [1500, 1520, 1490, 1550, 1580]
+  },
+  icici: {
+    name: "ICICI Bank",
+    sector: "Banking",
+    prices: [900, 920, 940, 930, 950]
+  },
+  sbi: {
+    name: "SBI",
+    sector: "Banking",
+    prices: [600, 620, 610, 630, 650]
+  },
+  airtel: {
+    name: "Bharti Airtel",
+    sector: "Telecom",
+    prices: [800, 820, 840, 830, 860]
+  },
+  wipro: {
+    name: "Wipro",
+    sector: "IT",
+    prices: [400, 410, 420, 415, 430]
+  },
+  hcl: {
+    name: "HCL Tech",
+    sector: "IT",
+    prices: [1100, 1120, 1150, 1130, 1170]
+  },
+  lnt: {
+    name: "Larsen & Toubro",
+    sector: "Construction",
+    prices: [2000, 2050, 2100, 2080, 2150]
+  },
+  itc: {
+    name: "ITC",
+    sector: "FMCG",
+    prices: [300, 310, 320, 315, 330]
+  },
+  maruti: {
+    name: "Maruti Suzuki",
+    sector: "Automobile",
+    prices: [9000, 9200, 9400, 9300, 9500]
+  },
+  tataMotors: {
+    name: "Tata Motors",
+    sector: "Automobile",
+    prices: [700, 720, 740, 730, 760]
+  },
+  sunpharma: {
+    name: "Sun Pharma",
+    sector: "Pharma",
+    prices: [1000, 1020, 1040, 1030, 1060]
+  },
+  ongc: {
+    name: "ONGC",
+    sector: "Energy",
+    prices: [150, 155, 160, 158, 165]
+  },
+  ntpc: {
+    name: "NTPC",
+    sector: "Power",
+    prices: [250, 255, 260, 258, 265]
+  },
+  adani: {
+    name: "Adani Enterprises",
+    sector: "Infrastructure",
+    prices: [2200, 2250, 2300, 2280, 2350]
+  },
+  asianPaints: {
+    name: "Asian Paints",
+    sector: "FMCG",
+    prices: [3000, 3050, 3100, 3080, 3150]
+  }
 };
 
 const canvas = document.getElementById("stockChart");
